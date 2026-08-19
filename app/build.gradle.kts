324b6ffe4479807be18632c3b12eb0afd05607b1
SpaceCalls0 <krewpro0987@gmail.com>
2026-08-19 23:30:25 +0300

    Add lifecycle-runtime-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+   implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
 
     // Compose BOM
